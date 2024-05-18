baa4f10d34e41a2f0617fa578337db9e59efa555
taehokwon111 <taehokwon48@gmail.com>
2024-05-18 14:34:20 +0900

    2024-05-18 study

---
+++ java/Algorithm/src/baek2884.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class baek2884 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] stChange = input.split(" ");
+        int a = Integer.parseInt(stChange[Math.toIntExact(0)]);
+        int b = Integer.parseInt(stChange[Math.toIntExact(1)]);
+        int nextTimeMin = b - 45;
+        if(nextTimeMin < 0){
+
+        }
+
+    // 아직 안품
+    }
+}
