70ebdb6617264a2722aed1dd90c917ee80886f26
taehokwon111 <taehokwon48@gmail.com>
2024-05-21 21:49:35 +0900

    2024-05-21 study

--- java/Algorithm/src/baek2525.java
+++ java/Algorithm/src/baek2525.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 
-public class baek2884 {
+public class baek2525 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        int input2 = scanner.nextInt();
         String[] stChange = input.split(" ");
         int a = Integer.parseInt(stChange[Math.toIntExact(0)]);
         int b = Integer.parseInt(stChange[Math.toIntExact(1)]);
-        b = b - 45;
+
+        b = b - input2;
+
         if (b < 0){
             b = b + 60;
             a = a - 1;
