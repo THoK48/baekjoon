2b15e327ad3fc7f2151247b2a899a56810a171d8
taehokwon111 <taehokwon48@gmail.com>
2024-05-23 22:49:08 +0900

    2024-05-23 study

--- java/Algorithm/src/baek2480.java
+++ java/Algorithm/src/baek2480.java
@@ -18,6 +18,8 @@ public class baek2480 {
             }
         } else if (b == c){
             System.out.println(1000 +(b * 100));
+        } else if(a == c){
+            System.out.println(1000 + (a * 100));
         } else {
             System.out.println((Math.max(Math.max(a,b),c)*100));
         }
